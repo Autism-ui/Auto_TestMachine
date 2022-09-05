7a57788ad4a4ce6ab575104c68b494ae5a6567d0
Autism_Huang <609886059@qq.com>
2022-09-05 14:44:25 +0800

    删除没必要的宏

--- ATM_STM32/Components/Support/sys.h
+++ ATM_STM32/Components/Support/sys.h
@@ -18,67 +18,74 @@
 
 /* boolean type definitions */
 #ifndef TRUE
-    #define TRUE                         1               /**< boolean true  */
+#define TRUE 1 /**< boolean true  */
 #endif
 
 #ifndef FALSE
-    #define FALSE                        0               /**< boolean fails */
-#endif
-
-#ifndef ENABLE
-    #define ENABLE                       1
-#endif
-
-#ifndef DISABLE
-    #define DISABLE                      0
+#define FALSE 0 /**< boolean fails */
 #endif
 
 #ifndef RAD_TO_DEG
-    #define RAD_TO_DEG 57.29f
+#define RAD_TO_DEG 57.29f
 #endif
 
-#define INT_STATE              uint32_t
+#define INT_STATE			   uint32_t
 #define MASTER_INT_STATE_GET() __get_PRIMASK()
-#define MASTER_INT_ENABLE()    do{__enable_irq();  }while(0)
-#define MASTER_INT_DISABLE()   do{__disable_irq(); }while(0)
-#define MASTER_INT_RESTORE(x)  do{__set_PRIMASK(x);}while(0)
+#define MASTER_INT_ENABLE() \
+	do {                    \
+		__enable_irq();     \
+	} while(0)
+#define MASTER_INT_DISABLE() \
+	do {                     \
+		__disable_irq();     \
+	} while(0)
+#define MASTER_INT_RESTORE(x) \
+	do {                      \
+		__set_PRIMASK(x);     \
+	} while(0)
 
 #define CRITICAL_SETCION_ENTER()                      \
-    do                                                \
-    {                                                 \
-        INT_STATE cpu_state = MASTER_INT_STATE_GET(); \
-        MASTER_INT_DISABLE();
+	do {                                              \
+		INT_STATE cpu_state = MASTER_INT_STATE_GET(); \
+		MASTER_INT_DISABLE();
 
-#define CRITICAL_SETCION_EXIT()        \
-        MASTER_INT_RESTORE(cpu_state); \
-    }                                  \
-    while (0)
+#define CRITICAL_SETCION_EXIT()    \
+	MASTER_INT_RESTORE(cpu_state); \
+	}                              \
+	while(0)
 
 #define var_cpu_sr() register unsigned long cpu_sr
 
-#define enter_critical()      \
-  do                          \
-  {                           \
-    cpu_sr = __get_PRIMASK(); \
-    __disable_irq();          \
-  } while (0)
+#define enter_critical()          \
+	do {                          \
+		cpu_sr = __get_PRIMASK(); \
+		__disable_irq();          \
+	} while(0)
 
-#define exit_critical()    \
-  do                       \
-  {                        \
-    __set_PRIMASK(cpu_sr); \
-  } while (0)
+#define exit_critical()        \
+	do {                       \
+		__set_PRIMASK(cpu_sr); \
+	} while(0)
 
 #define MUTEX_DECLARE(mutex) unsigned long mutex
-#define MUTEX_INIT(mutex)    do{mutex = 0;}while(0)
-#define MUTEX_LOCK(mutex)    do{__disable_irq();}while(0)
-#define MUTEX_UNLOCK(mutex)  do{__enable_irq();}while(0)
+#define MUTEX_INIT(mutex) \
+	do {                  \
+		mutex = 0;        \
+	} while(0)
+#define MUTEX_LOCK(mutex) \
+	do {                  \
+		__disable_irq();  \
+	} while(0)
+#define MUTEX_UNLOCK(mutex) \
+	do {                    \
+		__enable_irq();     \
+	} while(0)
 
-#define device_assert(EX)\
-  if (!(EX))           \
-  {                    \
-    log_assert("fail! %s %s %s", #EX, __FILE__, __LINE__);   \
-    while(1);          \
-  }
+#define device_assert(EX)                                      \
+	if(!(EX)) {                                                \
+		log_assert("fail! %s %s %s", #EX, __FILE__, __LINE__); \
+		while(1)                                               \
+			;                                                  \
+	}
 
-#endif // __SYS_INCLUDES_H__
+#endif	// __SYS_INCLUDES_H__
